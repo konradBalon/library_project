dbd67ddff3b9f43b649557078ce5426e2a32f451
konradBalon <konrad.balon@gmail.com>
2022-02-28 23:30:14 +0100

    following next pages + extra new fiels

--- crawler/items.py
+++ crawler/items.py
@@ -4,10 +4,10 @@
 # https://docs.scrapy.org/en/latest/topics/items.html
 
 import scrapy
+from itemloaders.processors import MapCompose
 from scrapy import Item, Field
-from scrapy.loader import ItemLoader
-from scrapy.loader.processors import TakeFirst
-
+from scrapy.loader.processors import TakeFirst, Join
+from w3lib.html import remove_tags
 
 class CrawlerItem(scrapy.Item):
     # define the fields for your item here like:
@@ -23,5 +23,7 @@ class BookItem(Item):
     category = Field(output_processor=TakeFirst(),)
     book_url = Field(output_processor=TakeFirst(),)
     book_img = Field(output_processor=TakeFirst(),)
+    description = Field(output_processor=Join(),)
+    pages = Field(output_processor=TakeFirst(),)
 
 
--- crawler/spiders/books.py
+++ crawler/spiders/books.py
@@ -6,8 +6,8 @@ from crawler.items import BookItem
 
 class BooksSpider(scrapy.Spider):
     name = 'books'
-    allowed_domains = ['www.lubimyczytac.pl/katalog', 'www.lubimyczytac.pl']
-    start_urls = ['https://lubimyczytac.pl/katalog/']
+    allowed_domains = ['www.lubimyczytac.pl', 'lubimyczytac.pl']
+    start_urls = ['https://lubimyczytac.pl/katalog']
 
     def parse(self, response):
         books_url = response.xpath("//a[@class='authorAllBooks__singleTextTitle float-left']/@href").getall()
@@ -15,9 +15,16 @@ class BooksSpider(scrapy.Spider):
         for url in books_url:
             yield scrapy.Request(url=f'https://www.lubimyczytac.pl{url}', callback=self.parse_book)
 
-    def parse_book(self, response):
-        l = ItemLoader(item=BookItem(), response=response)
+        next_page = response.xpath("//a[@class='page-link ml-0 stdPaginator btn']").xpath('@data-page').get()
 
+        if next_page is not None:
+            next_page = response.urljoin(f'?page={next_page}')
+            print(f'{next_page}' * 20)
+            yield scrapy.Request(next_page, callback=self.parse)
+
+    def parse_book(self, response):
+
+        l = ItemLoader(item=BookItem(), response=response)
         l.add_xpath('title', "//h1[@class='book__title']//text()")
         l.add_xpath('author', "//a[@class='link-name']/text()")
         l.add_xpath('author_url', "//a[@class='link-name']/@href")
@@ -26,12 +33,8 @@ class BooksSpider(scrapy.Spider):
         l.add_xpath('title', "//h1[@class='book__title']//text()")
         l.add_xpath('book_img', "//img[@class='img-fluid']/@src")
         l.add_value('book_url', response.url)
+        l.add_xpath('description', "//div[@class='collapse-content']//p//text()")
+        l.add_xpath('pages',
+                    "//span[@class='d-sm-inline-block book-pages book__pages pr-2 mr-2 pr-sm-3 mr-sm-3']/text()")
 
-
-        # title = response.xpath("//h1[@class='book__title']//text()").extract()[0].strip()
-        # author = response.xpath("//a[@class='link-name']/text()").extract()[0].strip()
-        # author_url = response.xpath("//a[@class='link-name']/@href").get()
-        # publisher = response.xpath("//span[@class='book__txt d-block d-xs-none mt-2 ']/a/text()").extract()[0].strip()
-        # category = response.xpath("//a[@class='book__category d-sm-block d-none']/text()").extract()[0].replace("\n",
-        #                                                                                                         "").strip()
         return l.load_item()
